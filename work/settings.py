3489cbe9bdddc114b8030c84c124a46c2d561d5b
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-04-02 02:50:40 +0600

    issue solv

--- work/settings.py
+++ work/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
@@ -136,21 +136,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /"staticfiles_build"
-STATICFILES_DIRS = [ STATIC_DIR ]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 
-LOGIN_URL = '/login/'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = MEDIA_DIR
+MEDIA_ROOT  = MEDIA_DIR
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
 
 # =====================> aamarpay payment method intergation <============
 

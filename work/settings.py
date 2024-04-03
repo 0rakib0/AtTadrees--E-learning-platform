9859c2ae266437b52ffec837be32eac18f54c3db
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-04-04 02:38:49 +0600

    add statis files

--- work/settings.py
+++ work/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,8 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = STATIC_DIR,
+STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = BASE_DIR /"staticfiles_build"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 

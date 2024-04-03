d584ba5764a6b73bbe99e4e679ef9bddee6be83e
rakib <hassanrakibul926@gmail.com>
2024-04-03 13:32:31 +0600

    add package.json file

--- work/settings.py
+++ work/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
@@ -137,8 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = STATIC_DIR,
 STATIC_ROOT = BASE_DIR /"staticfiles_build"
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 
 
